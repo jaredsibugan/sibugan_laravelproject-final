cc50b73ac66eef8871890c67de0a9f83f4b2d567
unknown <ashley@filamer.edu.ph>
2024-11-28 13:17:11 +0800

    final update

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentController;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $students = Student::all();
+    return view('dashboard', compact('students'));
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -16,5 +21,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/student/store', [StudentController::class, 'store'])->name('student.store');
+Route::delete('/student/{student}', [StudentController::class, 'destroy']) -> 
+name('student.destroy');
 
 require __DIR__.'/auth.php';
